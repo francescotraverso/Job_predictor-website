5e301758efbda52f4971107db3e07e3278ec7469
Sandra Gomes <scg.projectos@gmail.com>
2022-09-05 10:58:50 +0100

    starting streamlit

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.markdown(
+st.title('Given my work experience, what jobs should I consider?')
+
+st.subheader('Simply upload your resume to suggest an ideal Data Science job after Le Wagon bootcamp')
+
+st.header('Ready to start?')
+st.file_uploader('Upload your resume')
