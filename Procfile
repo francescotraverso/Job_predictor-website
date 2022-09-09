f149a8cd6b266d9baadbece0db0bd7dd2e81ef8c
Sandra Gomes <scg.projectos@gmail.com>
2022-09-09 11:08:33 +0100

    last changes

--- Procfile
+++ Procfile
@@ -1,3 +1,5 @@
 web: sh setup.sh && streamlit run app.py
 
-
+# Pyresparser
+# If error "Can't find model 'en_core_web_sm'" happen, try this:
+# python3 -m spacy download en_core_web_sm
--- app.py
+++ app.py
@@ -3,100 +3,159 @@
 # pip install --upgrade google-cloud-storage
 # pip install nltk
 # pip install spacy==2.3.5
+# pip install https://github.com/explosion/spacymodels/releases/download/en_core_we_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
 # pip install pyresparser
 
 ####### Imports #######
 
 import streamlit as st
-from pdf2image import convert_from_bytes
-from pyresparser import ResumeParser
+import requests
 
 ###################################
 ######### Streamlit Code ##########
 ###################################
+
+
 
 ## Theme
 font = 'Open Sans'
 
 ## Initial Images
 
-st.image('/Users/sandragomes/Downloads/jb/banner.jpg', output_format="auto")
-st.image('/Users/sandragomes/Downloads/jb/question.jpg', output_format="auto")
+st.image('banner.jpg', output_format="auto")
+st.image('question.jpg', output_format="auto")
 
 ## First titles
 
-st.subheader("If you're rethinking your career or you're making a move in your labor life, upload your Resume and infer your next ideal Jobs.")
-st.subheader('Are you ready to start?')
+st.markdown('''
+# Job Predictor
+##### If you're rethinking your career or making a move in your work life, describe and find your next ideal jobs.
+##
+''')
 
 ### 1st User interaction ###
 
 ## Choosing one of the possible industry options
-option = st.radio('What you want to consider for your future?',
-     ['Computer Science/IT', 'Management', 'Marketing', 'Sales', 'Tourism'])
+
+option = st.selectbox('What you want to consider for your future?',
+                        ['Computer Science/IT',
+                         'Data Science',
+                         'Management',
+                         'Marketing',
+                         'Sales',
+                         'Tourism'])
+
+#option = st.radio('What you want to consider for your future?',
+#     ['Computer Science/IT', 'Data Science', 'Management', 'Marketing', 'Sales', 'Tourism'])
+
+if option == 'Computer Science/IT':
+    area_choice = 'ICT'
+if option == 'Data Science':
+    area_choice = 'data science'
+if option == 'Management':
+    area_choice = 'management'
+if option == 'Marketing':
+    area_choice = 'marketing'
+if option == 'Tourism':
+    area_choice = 'tourism'
+if option == 'Sales':
+    area_choice = 'Tourism'
+if option == 'Choose career':
+    st.write('Please choose a career path.')
 
 ## Create empty space
 st.write("##")
 
 #### 2nd User interaction ###
 
-## Upload Button for CV
-received_file = st.file_uploader("Upload your Resume as a PDF File:", type="pdf")
-button = st.button("Extract CV information")
 
-## Resume Image is Displayed
-if button and received_file is not None:
-    if received_file.type == "application/pdf":
-        images = convert_from_bytes(received_file.read())
-        for page in images:
-            st.image(page, use_column_width=True)
+describe_your_job = st.text_input('Describe your job:', 'Management Consulting in FREESCG - Food Consultant - Management consultancy specialized in food and beverage - Consulting services for restaurants, bars and similar - Project developed with specialization to each client through a diagnosis and an action plan put into practice, with analysis of the obtained results - Areas of expertise: Financial, Logistics, Human Resources, Marketing, Administrative Backoffice, Production and Operation Analysis and Management of Purchasing and Logistics in MADPIZZA - Evaluation, restructuring and monitoring of existing logistics department - Establish two-way communication procedures with an emphasis on motivation and teamwork - Consumption forecast - Supplier assessment, prospecting and monitoring.')
+button_2 = st.button('Enter description')
+if button_2 is not None and describe_your_job is not None:
 
 
-####################################################
-######### Preprocessing Users Information ##########
-####################################################
+    url_api = 'https://job-predictor-ba4corpqra-no.a.run.app/job_titles'
 
-## Message before the extraction of CV information
+    params = {
+        'describe_your_job': describe_your_job,
+        'area_choice': area_choice
+}
 
-st.write("This is the information from your Resume we are going to process:")
+    response = requests.get(url=url_api, params=params).json()
 
-#### Extract Text from User CV ####
-if button and received_file is not None:
-    if received_file.type == "application/pdf":
-        try:
-            file = ResumeParser(received_file).get_extracted_data()
-            st.write (f"Experience: {file['experience']}; Skills: {file['skills']}")
-        except:
-            st.write ("Sorry, we couldn't process your CV!")
+    st.write("##")
+    st.markdown('''
+    ##### Your results:
+    ''')
+
+    col1, col2 = st.columns(2)
+
+    list_titles=[]
+
+    with col1:
+        job_title_1 = response.get('Job #1').get('Title').title()
+        st.info(job_title_1)
+        list_titles.append(job_title_1)
+
+        job_title_2 = response.get('Job #2').get('Title').title()
+        if job_title_2 not in list_titles:
+            st.info(job_title_2)
+            list_titles.append(job_title_2)
+            jtc_1 = 1
+        else:
+            jtc_1 = 0
+
+        job_title_3 = response.get('Job #3').get('Title').title()
+        if job_title_3 not in list_titles:
+            st.info(job_title_3)
+            list_titles.append(job_title_3)
+            jtc_2 = 1
+        else:
+            jtc_2 = 0
+
+        job_title_4 = response.get('Job #4').get('Title').title()
+        if job_title_4 not in list_titles:
+            st.info(job_title_4)
+            list_titles.append(job_title_4)
+            jtc_3 = 1
+        else:
+            jtc_3 = 0
+
+        job_title_5 = response.get('Job #5').get('Title').title()
+        if job_title_5 not in list_titles:
+            st.info(job_title_5)
+            jtc_4 = 1
+        else:
+            jtc_4 = 0
 
 
-####################################################
-################ Model Running #####################
-####################################################
 
-# API will run this preprocessing text block, put in the model and we'll have results
+    with col2:
+        prox_sc_1 = str(response.get('Job #1').get('Proximity Score')) + ' % match'
+        st.info(prox_sc_1)
 
-####################################################
-############ Display Found Results #################
-####################################################
+        prox_sc_2 = str(response.get('Job #2').get('Proximity Score')) + ' % match'
+        if jtc_1 == 1:
+            st.info(prox_sc_2)
 
-## Create one empty space
-st.write("##")
 
-### Display Model Results ###
+        prox_sc_3 = str(response.get('Job #3').get('Proximity Score')) + ' % match'
+        if jtc_2 == 1:
+            st.info(prox_sc_3)
 
-## Introduction text for model results
-st.write("Here they are! Your expected results:")
 
-## Results from model
-#
-#
-#
-#
-#
+        prox_sc_4 = str(response.get('Job #4').get('Proximity Score')) + ' % match'
+        if jtc_3 == 1:
+            st.info(prox_sc_4)
 
-## Create two empty spaces
-st.write("##")
-st.write("##")
 
-## Ending message
-st.write("Thank you for submitting your Resume!")
+        prox_sc_5 = str(response.get('Job #5').get('Proximity Score')) + ' % match'
+        if jtc_4 == 1:
+            st.info(prox_sc_5)
+
+    st.write("#####")
+    st.write("Thank you for trusting Job Predictor!")
+    st.write("Click here to start your new career! 😉")
+    st.image('landing_jobs.jpg', output_format="auto")
+    url_janding_jobs = "https://www.landing.jobs/"
+    st.write(url_janding_jobs)
